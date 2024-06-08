65f1596d9d2894ec1058452d2e0fb4b64c9b40e7
Hemanth <hemanththyagaraj@gmail.com>
2024-06-08 20:30:07 +0530

    Initial commit 💥

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const http = require("http")
+const express = require("express")
+const cors = require("cors")
 
-const PORT = 8000
+const app = express()
+const PORT = process.env.PORT || 8000;
+
+app.use(cors({
+    origin: "*"
+}))
 
 let notes = [
     {
-      id: 1,
-      content: "HTML is easy",
-      important: true
+        id: 1,
+        content: "HTML is easy",
+        important: true,
     },
     {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false,
     },
     {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true,
+    },
+];
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "application/json" })
-    res.end(JSON.stringify(notes))
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>")
 })
 
-server.listen(PORT)
+app.get("/notes", (req, res) => {
+    return res.status(200).json(notes)
+})
+
+app.listen(PORT, () => {
+    console.log("Listening on port " + PORT)
+})
